b0d23249ecda32d5a21915f50a9d474c5d6d4800
Suyash-Pradhan <suyashpradhann@gmail.com>
2025-09-06 19:03:52 +0530

    Refactor user model: standardize formatting and improve code readability

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,83 +5,84 @@ import jwt from 'jsonwebtoken'
 
 
 const UserSchema = new mongoose.Schema({
-    username:{
-        type:String,
-        required:true,
-        unique:true,
-        lowecase:true,
-        trim:true,
-        index:true //to make serchable - so that it can come in DB search
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowecase: true,
+        trim: true,
+        index: true //to make serchable - so that it can come in DB search
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        trim:true
-        
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+
     },
-    fullname:{
-        type:String,
-        required:true,
-        unique:true,
-        trim:true
-        
+    fullname: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+
     },
-    avatar:{
-        type:String,
-        required:true,
+    avatar: {
+        type: String,
+        required: true,
     },
-    coverImage:{
-        type:String,
+    coverImage: {
+        type: String,
     },
-    watchHistory:{
-        type:Schema.Types.ObjectId,
-        ref:'Video'
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Video'
+        }],
+    password: {
+        type: String,
+        required: [true, 'password zaruri hai']
     },
-    password:{
-        type:String,
-        required:[true,'password zaruri hai']
-    },
-    refreshToken:{
-        type:String,
-        
+    refreshToken: {
+        type: String,
+
     }
-},{timestamps:true})
+}, { timestamps: true })
 
-UserSchema.pre("save",async function (next){
-    if(this.isModified("password")){
+UserSchema.pre("save", async function (next) {
+    if (this.isModified("password")) {
 
-        this.password = await bcrypt.hash(this.password,10)
+        this.password = await bcrypt.hash(this.password, 10)
         next()
     }
-    else{return}
+    else { return }
 })
-UserSchema.methods.isPasswordCorrect = async function (password){
-  return await  bcrypt.compare(password,this.password)
+UserSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
 }
-UserSchema.methods.generateAccessToken = function(){
+UserSchema.methods.generateAccessToken = function () {
     return jwt.sign(
         {
-            _id:this._id,
-            username:this.username,
-            fullname:this.fullname,
-            email:this.email
+            _id: this._id,
+            username: this.username,
+            fullname: this.fullname,
+            email: this.email
         },
-        process.env.ACCESS_TOKEN_SECRET,{
-            expiresIn:process.env.ACCESS_TOKEN_EXPIARY
-        }
+        process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIARY
+    }
     )
 }
-UserSchema.methods.generateRefreshToken = function(){
+UserSchema.methods.generateRefreshToken = function () {
     return jwt.sign(
         {
-            _id:this._id,
-            
+            _id: this._id,
+
         },
-        process.env.REFRESH_TOKEN_SECRET,{
-            expiresIn:process.env.REFRESH_TOKEN_EXPIARY
-        }
+        process.env.REFRESH_TOKEN_SECRET, {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIARY
+    }
     )
 }
 
-export const User = mongoose.model('user',UserSchema)
+export const User = mongoose.model('user', UserSchema)
